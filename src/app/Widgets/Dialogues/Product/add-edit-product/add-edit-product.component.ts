fcc82db7669318842cb125b402207ac6d6392f72
amr.mosaad <Eng_amrmossad93>
2022-08-22 20:19:29 +0200

    NF:Interceptor

--- src/app/Widgets/Dialogues/Product/add-edit-product/add-edit-product.component.ts
+++ src/app/Widgets/Dialogues/Product/add-edit-product/add-edit-product.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-edit-product',
